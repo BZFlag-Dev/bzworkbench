fe74efcdf78348d2b01c3a66fb46d45aeab531cd
Jeff Myers <jeffm2501@users.sourceforge.net>
2010-03-16 00:24:13 +0000

    remove eclipse build system ( nothing else in BZ uses it) Add todo mentioning need for proper CLI build system
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/bzworkbench@20921 08b3d480-bf2c-0410-a26f-811ee3361c24

--- Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-LIBS := -lfltk -lfltk_images -losg -losgViewer -lfltk_gl -lGL -lGLU
-
-USER_OBJS :=
